1d4960fbaf9d4af2f8757540f728f6ae27184d14
NONE <none@valvesoftware.cc>
2022-07-15 19:28:12 +0200

    Added Programm Base
    Programm Base
    
    Co-Authored-By: SatsuyaSystems <78853027+SatsuyaSystems@users.noreply.github.com>

---
+++ Application/BeeSSH/BeeSSH/DiscordRPC/DiscordRPC_Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeSSH.DiscordRPC
+{
+    internal class DiscordRPC_Config
+    {
+    }
+}
---
+++ Application/BeeSSH/BeeSSH/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using Syncfusion.Windows.Shared;
+
+namespace BeeSSH
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}

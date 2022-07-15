0e72a004d4ee4973a5b86517b0bccfe8008c0f97
NONE <none@valvesoftware.cc>
2022-07-15 18:33:42 +0200

    Update app.js

--- Webpage/app.js
+++ Webpage/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const flash = require('connect-flash');
@@ -8,7 +7,7 @@ const helmet = require("helmet");
 require('dotenv').config();
 const app = express();
 
-const allowedMethods = ['GET', 'HEAD', 'POST'];
+const allowedMethods = ['GET', 'POST'];
 // Passport Config
 require('./config/passport')(passport);
 
@@ -17,14 +16,14 @@ const db = process.env.MONGODBURL;
 
 // Connect to MongoDB
 mongoose
-  .connect( db
-  )
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
 // EJS
-app.use(expressLayouts);
-app.use(helmet());
+app.use(helmet({
+    contentSecurityPolicy: false,
+}));
 app.set('view engine', 'ejs');
 app.set('views', [__dirname + '\\views_userpandel', __dirname + '\\view_login',
     __dirname + '\\view_global', __dirname + '\\view_admin']);
@@ -35,7 +34,7 @@ app.use(express.urlencoded({ extended: true }));
 // Express session
 app.use(
   session({
-    secret: [process.env.SESSIONSECRET,process.env.SESSIONSECRET_1, process.env.SESSIONSECRET_2],
+    secret: [process.env.SESSIONSECRET, process.env.SESSIONSECRET_1, process.env.SESSIONSECRET_2],
       name: 'beessh',
       cookie: {
         sameSite: true, maxAge: 86400 * 1000
@@ -52,7 +51,6 @@ app.use((req, res, next) => {
     if (!allowedMethods.includes(req.method)) {
         return res.end(401);
     }
-    res.removeHeader('x-powered-by');
     next()
 });
 

7de7ffe24d3712021ed0f36e8195edaae062b0ff
NONE <none@valvesoftware.cc>
2022-07-16 15:00:27 +0200

    EMail

---
+++ Webpage/config/EMAIL_ResetPasswort.js
@@ -0,0 +1,29 @@
+const nodemailer = require('nodemailer');
+const fs = require('fs');
+
+module.exports = function (username, useremail) {
+    const email = fs.readFile("./emails/PasswortReset.html", (err) => {console.log(err)}).toString();
+    const emailData = email.replace("{USERNAME}", username);
+
+
+    const smtpTransport = nodemailer.createTransport(smtpTransport({
+        host: process.env.EMAILHOST,
+        secure: true,
+        port: process.env.MAILPORT,
+        auth: {
+            user: process.env.MAILUSERNAME,
+            pass: process.env.MAILUSERPASS
+        }
+    }));
+    const mailOptions = {
+        from: process.env.MAILUSERNAME,
+        to : useremail,
+        subject : 'Reset Password',
+        html : emailData
+    };
+    smtpTransport.sendMail(mailOptions, function (error, response) {
+        if (error) {
+            console.log(error);
+        }
+    });
+}

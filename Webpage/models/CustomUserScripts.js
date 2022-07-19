9be64a5797a8f767fe53244f7a02a27aeab948d2
NONE <none@valvesoftware.cc>
2022-07-19 15:30:41 +0200

    Create CustomUserScripts.js

---
+++ Webpage/models/CustomUserScripts.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const UserScripte = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    UID: {
+        type: String,
+        required: true
+    },
+    Script: {
+        type: String,
+        required: true
+    }
+});
+
+const UserSSHScript = mongoose.model('UserSSHScripte', UserScripte);
+
+module.exports = UserSSHScript;

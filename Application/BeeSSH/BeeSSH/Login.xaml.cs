1e128f06e303bbff5b712500e9a84654ab624efe
HakuSystems <66133638+HakuSystems@users.noreply.github.com>
2022-08-12 20:49:38 +0200

    Improved Login Window

--- Application/BeeSSH/BeeSSH/Login.xaml.cs
+++ Application/BeeSSH/BeeSSH/Login.xaml.cs
@@ -1,18 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BeeSSH
 {
@@ -50,13 +38,20 @@ namespace BeeSSH
 
         private void exitApp(object sender, RoutedEventArgs e)
         {
-            Environment.Exit(0);
+            Close();
         }
 
         private void dragmove(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
-                this.DragMove();
+                DragMove();
         }
+
+        private void minBtn_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+
+
+        private void twoFAEnabled_Checked(object sender, RoutedEventArgs e) => faAuthBox.Visibility = Visibility.Visible;
+
+        private void twoFAEnabled_Unchecked(object sender, RoutedEventArgs e) => faAuthBox.Visibility = Visibility.Collapsed;
     }
 }

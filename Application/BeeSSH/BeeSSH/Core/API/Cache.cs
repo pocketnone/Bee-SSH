85f20a956fbae5754c2afede886f11712a5d90aa
NONE <none@valvesoftware.cc>
2022-08-18 10:54:17 +0200

    Added Stuff

--- Application/BeeSSH/BeeSSH/Core/API/Cache.cs
+++ Application/BeeSSH/BeeSSH/Core/API/Cache.cs
@@ -8,9 +8,29 @@ namespace BeeSSH.Core.API
 {
     internal class Cache
     {
-        internal static string UserNameFromAPI { get; set; } 
         internal static string AuthCookieForAPI { get; set; }
-        internal static Dictionary<string, Dictionary<string, string>> APICacheServers { get; set; }
+        internal static string EncryptionMasterPass { get; set; }                           // Master Password for encryption
+        internal static List<ServerListModel> ServerList = new List<ServerListModel>();     // All Servers
 
+        // API Endpoints 
+        internal static string LoginAPIURL = "https://as.mba/api/client_login";             // Request a Login
+                
+        internal static string AddServerAPIURL = "https://as.mba/api/client_new";           // Add a new server to the Webapp
+
+        internal static string FetchScriptsAPIURL = "https://as.mba/api/fetch_userscripte"; 
+
+        internal static string AddScriptAPIURL = "https://as.mba/api/add_userscript";
+
+        // Some stuff
+        internal static string ClientAuthKey = "vnsjrgshvb48sbvrbiwbkb_kbkkbhkgberibg";
+    }
+
+    internal class ServerListModel
+    {
+        internal string ServerName           { get; set; }
+        internal string ServerIP             { get; set; }
+        internal string ServerPassword       { get; set; }
+        internal string ServerPort           { get; set; }
+        internal byte[] RSAKEY               { get; set; }
     }
 }
--- Application/BeeSSH/BeeSSH/Core/API/Request.cs
+++ Application/BeeSSH/BeeSSH/Core/API/Request.cs
@@ -1,12 +1,123 @@
 ﻿using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using static BeeSSH.Core.Crypter.String;
+using static BeeSSH.Core.API.Cache;
 
 namespace BeeSSH.Core.API
 {
     internal class Request
+    {   
+
+        internal static string Login(string username,string password,string totp = "000000")
+        {
+            var requestOptions = new Dictionary<string, string>();
+            requestOptions.Add("tool", ClientAuthKey);
+            requestOptions.Add("email", username);
+            requestOptions.Add("password", password);
+            requestOptions.Add("otp", totp);
+            using(var client = new HttpClient())
+            {
+                try
+                {
+
+                    var req = new HttpRequestMessage(HttpMethod.Post, LoginAPIURL) { Content = new FormUrlEncodedContent(requestOptions) };     // Request
+                    var res_raw = client.SendAsync(req).Result;                                                                                 // Response from the API
+                    string res = res_raw.Content.ReadAsStringAsync().Result;                                                                    // Convert to String
+                    var datastuff = JsonConvert.DeserializeObject<LoginDeserilizeModel>(res);                                                   // Convert to responseapi
+
+                    // if Error
+                    if(!System.String.IsNullOrEmpty(datastuff.Error))
+                    {
+                        return datastuff.Error;
+                    }
+
+
+                    AuthCookieForAPI = datastuff.AuthCookie;
+                    foreach (var item in datastuff.ServerRes)
+                    {
+                        if(item.isKey == "false")
+                        {
+                            ServerList.Add(new ServerListModel
+                            {
+                                ServerName = Decrypt(item.Name, EncryptionMasterPass),
+                                ServerIP = Decrypt(item.ServerIP, EncryptionMasterPass),
+                                ServerPassword = Decrypt(item.serverpass, EncryptionMasterPass),
+                                ServerPort = Decrypt(item.port, EncryptionMasterPass)
+                            });
+                        } else
+                        {
+                            ServerList.Add(new ServerListModel
+                            {
+                                ServerName = Decrypt(item.Name, EncryptionMasterPass),
+                                ServerIP = Decrypt(item.ServerIP, EncryptionMasterPass),
+                                ServerPort = Decrypt(item.port, EncryptionMasterPass),
+                                RSAKEY = Encoding.UTF8.GetBytes(Decrypt(item.serverpass, EncryptionMasterPass))
+                            });
+                        }
+                    }
+                    return "ok";
+                } catch
+                {
+                    return "Error";
+                }
+            }
+        }
+
+        // Add a Server to the Backend from the Webpage
+        internal static string AddServer(string servername_crypted, string port_crypted, bool isKey, string ipadress_crypted, string PasswordOrKey_crypted)
+        {
+            var requestOptions = new Dictionary<string, string>();
+            requestOptions.Add("tool", ClientAuthKey);
+            requestOptions.Add("authkey", AuthCookieForAPI);
+            requestOptions.Add("servername", servername_crypted);
+            requestOptions.Add("port", port_crypted);
+            requestOptions.Add("isKEY", isKey.ToString());
+            requestOptions.Add("ipadress", ipadress_crypted);
+            requestOptions.Add("PasswordKey", PasswordOrKey_crypted);
+            using (var client = new HttpClient())
+            {
+                var req = new HttpRequestMessage(HttpMethod.Post, AddServerAPIURL) { Content = new FormUrlEncodedContent(requestOptions) };     // Request
+                var res_raw = client.SendAsync(req).Result;                                                                                 // Response from the API
+                string res = res_raw.Content.ReadAsStringAsync().Result;                                                                    // Convert to String
+                var datastuff = JsonConvert.DeserializeObject<OtherResponse>(res);
+                return datastuff.DataRes;
+            }
+        }
+    }
+
+    // Login
+    internal class LoginDeserilizeModel
     {
+        [JsonProperty("Info")]
+        public string Error { get; set; }
+        [JsonProperty("AuthKey")]
+        public string AuthCookie { get; set; }
+        [JsonProperty("AuthKey")]
+        public List<DataLoginModel> ServerRes { get; set; }
+    }
+    // Login
+    internal class DataLoginModel {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("crpyt_ip")]
+        public string ServerIP { get; set; }
+        [JsonProperty("crpyt_password")]
+        public string serverpass { get; set; }
+        [JsonProperty("crpyt_port")]
+        public string port { get; set; }
+        [JsonProperty("isKey")]
+        public string isKey { get; set; }
+    }
+
+    // ETC
+    internal class OtherResponse
+    {
+        [JsonProperty("Info")]
+        public string DataRes { get; set; }
     }
 }

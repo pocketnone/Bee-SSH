ba712d2f37121709f3b98ab05187f91acbe83cae
NONE <none@valvesoftware.cc>
2022-08-20 22:21:27 +0200

    Added Selection for RSA

--- Application/BeeSSH/BeeSSH/Interface/UserControlls/AddServerUserControl.xaml.cs
+++ Application/BeeSSH/BeeSSH/Interface/UserControlls/AddServerUserControl.xaml.cs
@@ -5,6 +5,11 @@ using static BeeSSH.Core.API.Request;
 using static BeeSSH.Core.API.Cache;
 using static BeeSSH.Core.Crypter.String;
 using BeeSSH.Interface.CustomMessageBox;
+using WK.Libraries.BetterFolderBrowserNS;
+using System.IO;
+using System.Windows.Forms;
+using OpenFileDialog = System.Windows.Forms.OpenFileDialog;
+using UserControl = System.Windows.Controls.UserControl;
 
 namespace BeeSSH.Interface.UserControlls
 {
@@ -13,7 +18,7 @@ namespace BeeSSH.Interface.UserControlls
     /// </summary>
     public partial class AddServerUserControl : UserControl
     {
-        byte[] rsakey = null; 
+        byte[] rsakey_buff = null; 
         public AddServerUserControl()
         {
             InitializeComponent();
@@ -22,21 +27,28 @@ namespace BeeSSH.Interface.UserControlls
 
         private void AddRSABtn(object sender, RoutedEventArgs e)
         {
-
-            
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "BEESSH | Select RSA File";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                string filePath = ofd.FileName;
+                rsakey_buff = File.ReadAllBytes(filePath);
+            }
+            ofd.Dispose();
         }
 
         private void AddServerBtn(object sender, RoutedEventArgs e)
         {
             string response = "Not Requested";
-            if (rsakey == null && string.IsNullOrEmpty(ServerPassPharse.Text))
+            if (rsakey_buff == null && string.IsNullOrEmpty(ServerPassPharse.Text))
             {
                 response = AddServer(Encrypt(ServerName.Text, EncryptionMasterPass), Encrypt(ServerPort.Text, EncryptionMasterPass),
                false, Encrypt(ServerIP.Text, EncryptionMasterPass), Encrypt(ServerPassword.Password, EncryptionMasterPass), 
                Encrypt(Serverusername.Text, EncryptionMasterPass),
                Encrypt(ServerPassPharse.Text, EncryptionMasterPass));
             }
-            else if (rsakey == null)
+            else if (rsakey_buff == null)
             {
                 response = AddServer(Encrypt(ServerName.Text, EncryptionMasterPass), Encrypt(ServerPort.Text, EncryptionMasterPass),
                false, Encrypt(ServerIP.Text, EncryptionMasterPass), Encrypt(ServerPassword.Password, EncryptionMasterPass),

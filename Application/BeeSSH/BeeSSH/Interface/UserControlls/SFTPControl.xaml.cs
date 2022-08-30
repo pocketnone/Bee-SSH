e9fe308553bdafe787ad41f7525ae443c756676d
NONE <none@valvesoftware.cc>
2022-08-30 14:09:18 +0200

    Updated UI

---
+++ Application/BeeSSH/BeeSSH/Interface/UserControlls/SFTPControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace BeeSSH.Interface.UserControlls
+{
+    public partial class SFTPControl : UserControl
+    {
+        public SFTPControl()
+        {
+            InitializeComponent();
+        }
+    }
+}

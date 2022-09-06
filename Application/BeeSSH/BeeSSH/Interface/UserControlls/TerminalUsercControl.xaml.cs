d3e1dc6479b42de95974588a265640fd7265a666
NONE <none@valvesoftware.cc>
2022-09-06 15:47:55 +0200

    Update TerminalUsercControl.xaml.cs

--- Application/BeeSSH/BeeSSH/Interface/UserControlls/TerminalUsercControl.xaml.cs
+++ Application/BeeSSH/BeeSSH/Interface/UserControlls/TerminalUsercControl.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows.Controls;
 using BeeSSH.Core.API;
+using BeeSSH.Interface.CustomMessageBox;
 using Renci.SshNet;
 using static BeeSSH.Core.API.Cache;
 using static BeeSSH.Utils.DiscordRPC.DiscordRPCManager;
@@ -20,9 +22,56 @@ namespace BeeSSH.Interface.UserControlls
             Terminal_MainView();
         }
 
-        private void ConnectSSH()
+        private void ConnectSSHWithOutRSA()
         {
             var b = Cache.ServerList.Find(x => x.ServerUID.Contains(_Servername));
+            byte[] expectedFingerPrint = new byte[] { };
+            bool fingerprint = false;
+            if (b.FingerPrint != "null")
+            {
+                expectedFingerPrint = Convert.FromBase64String(b.FingerPrint);
+                fingerprint = true;
+            }
+
+            var client = new SshClient(b.ServerIP, Int32.Parse(b.ServerPort), b.ServerUserName);
+            client.HostKeyReceived += (sender, e) =>
+            {
+                if (fingerprint)
+                {
+                    if (expectedFingerPrint.Length == e.FingerPrint.Length)
+                    {
+                        for (var i = 0; i < expectedFingerPrint.Length; i++)
+                        {
+                           
+                            if (expectedFingerPrint[i] != e.FingerPrint[i])
+                            {
+                                bool trust = Convert.ToBoolean(new BeeFingerprint($"The Server {b.ServerName} has a differnet fingerprint"
+                                    , b.ServerName).ShowDialog());
+
+                               if(!trust) {
+                                    e.CanTrust = false;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        bool trust = Convert.ToBoolean(new BeeFingerprint($"The Server {b.ServerName} has a differnet fingerprint"
+                            , b.ServerName).ShowDialog());
+                        if(!trust)
+                        {
+                            e.CanTrust = false;
+                        }
+                    }
+                }
+            };
+            client.Connect();
+            
+        }
+
+        private void ConnectSSHwithRSA()
+        {
             
         }
     }
